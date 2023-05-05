01c569e5a5e981f477230f3b8a24c5786eb43673
Nikkie <2859008466@qq.com>
2023-05-05 12:03:22 +0800

    delete data_group molecules

--- new_runnerfile.bat
+++ new_runnerfile.bat
@@ -1,8 +1,8 @@
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.4
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.5
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.6
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.7
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.8
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.4
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.5
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.6
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.7
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.8
 
 @REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1
 @REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3
