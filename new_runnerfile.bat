1022b6ccd7f2b6eabca1ad2dd16ed1de52573b3d
Nikkie <2859008466@qq.com>
2023-05-05 11:53:36 +0800

    try smaller dist_threshold

--- new_runnerfile.bat
+++ new_runnerfile.bat
@@ -1,41 +1,58 @@
+python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.4
+python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.5
+python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.6
+python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.7
+python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.8
 
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5
 
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 1.1
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 1.1
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 1.1
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 1.1
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 1.1
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 1.1
 
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.9
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 0.9
-python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 0.9
-
-
-python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1
-python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3
-python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5
-
-python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 1.1
-python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 1.1
-python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 1.1
-
-python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.9
-python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 0.9
-python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 0.9
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.9
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 0.9
+@REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 0.9
 
 
-python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1
-python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3
-python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5
+python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.4
+python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.5
+python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.6
+python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.7
+python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.8
 
-python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 1.1
-python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 1.1
-python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 1.1
 
-python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.9
-python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 0.9
-python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 0.9
+@REM python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3
+@REM python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5
+
+@REM python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 1.1
+@REM python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 1.1
+@REM python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 1.1
+
+@REM python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.9
+@REM python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 0.9
+@REM python main_multiDS.py --repeat 1 --data_group biochem --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 0.9
+
+python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.4
+python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.5
+python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.6
+python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.7
+python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.8
+
+
+@REM python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1
+@REM python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3
+@REM python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5
+
+@REM python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 1.1
+@REM python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 1.1
+@REM python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 1.1
+
+@REM python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 1 --dist_threshold 0.9
+@REM python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 3 --dist_threshold 0.9
+@REM python main_multiDS.py --repeat 1 --data_group mix --seed 10  --seq_length 5 --num_rounds 50 --comm_threshold 1 --local_epoch 5 --dist_threshold 0.9
 
 @REM python main_multiDS.py --repeat 1 --data_group molecules --seed 10 --epsilon1 0.08 --epsilon2 0.4 --seq_length 5 --num_rounds 50 --comm_threshold 3
 @REM python main_multiDS.py --repeat 1 --data_group biochem --seed 10 --epsilon1 0.08 --epsilon2 0.4 --seq_length 5 --num_rounds 50 --comm_threshold 3
